96d60a19dfd5d152bea7a843b06186798421a169
rehman-areeba <areebareh2004@gmail.com>
2026-05-10 12:20:32 -0700

    feat(ui): add React Router with protected routes and role guards

---
+++ banking-ui/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export const LoginPage = () => <div>Login Page</div>;
+export const RegisterPage = () => <div>Register Page</div>;
+export const DashboardPage = () => <div>Dashboard Page</div>;
+export const TransferPage = () => <div>Transfer Page</div>;
+export const TransactionsPage = () => <div>Transactions Page</div>;
+export const AdminPage = () => <div>Admin Page</div>;
+export const NotFoundPage = () => <div>404 - Not Found</div>;
